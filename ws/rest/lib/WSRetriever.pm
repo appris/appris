89fb56789b290d1de8ba47a5e9081739a16c3e74
josemrc <josemrc@gmail.com>
2015-09-11 08:17:25 +0200

    Genome Browser Visualization

--- ws/rest/lib/WSRetriever.pm
+++ ws/rest/lib/WSRetriever.pm
@@ -1669,11 +1669,12 @@ sub get_gen_features {
 	if ( defined $ids ) {
 		$optional_params .= '%26'.'ids='.$ids;
 	}
-	my ($browser_params) =  '%26'.'headbed=yes:ensGene,ccdsGene';
-	if ( ( ($species_id eq 'homo_sapiens') or ($species_id eq 'mus_musculus') ) and ($ens > 74) ) {
-		$browser_params = '%26'.'headbed=yes:wgEncodeGencodeV'.$gencode_db. ',wgEncodeGencodeCompV'.$gencode_db. ',ccdsGene';			
-	}		
-	my ($query) = 'http://' . CGI->new()->server_name() . '/rest/' . $query_id . '?' . $required_params . $optional_params . $browser_params;
+	#my ($browser_params) =  '%26'.'headbed=yes:ensGene,ccdsGene';
+	#if ( ( ($species_id eq 'homo_sapiens') or ($species_id eq 'mus_musculus') ) and ($ens > 74) ) {
+	#	$browser_params = '%26'.'headbed=yes:wgEncodeGencodeV'.$gencode_db. ',wgEncodeGencodeCompV'.$gencode_db. ',ccdsGene';			
+	#}
+	#my ($query) = 'http://' . CGI->new()->server_name() . '/rest/' . $query_id . '?' . $required_params . $optional_params . $browser_params;
+	my ($query) = 'http://' . CGI->new()->server_name() . '/rest/' . $query_id . '?' . $required_params . $optional_params ;
 	
 	# make a request to render tracks of UCSC
 	my ($params) = 'db=' . $ucsc_db;
@@ -1681,13 +1682,22 @@ sub get_gen_features {
 	$params .= '&' . 'hgt.labelWidth=' . '20';
 	$params .= '&' . 'pix=' . '1100';
 	$params .= '&' . 'hgt.customText=' . $query;
-	$params .= '&' . 'wgEncodeGencodeCompV'.$gencode_db.'_sel=' . '1';
-	$params .= '&' . 'wgEncodeGencodeBasicV'.$gencode_db.'_sel=' . '0';
-	$params .= '&' . 'wgEncodeGencodePseudoGeneV'.$gencode_db.'_sel=' . '0';
-	$params .= '&' . 'wgEncodeGencodeCompV'.$gencode_db.'.label=' . 'accession';
+	#$params .= '&' . 'wgEncodeGencodeCompV'.$gencode_db.'_sel=' . '1';
+	#$params .= '&' . 'wgEncodeGencodeBasicV'.$gencode_db.'_sel=' . '0';
+	#$params .= '&' . 'wgEncodeGencodePseudoGeneV'.$gencode_db.'_sel=' . '0';
+	#$params .= '&' . 'wgEncodeGencodeCompV'.$gencode_db.'.label=' . 'accession';
 	$params .= '&' . 'hideTracks=1';
 	$params .= '&' . 'ccdsGene=full';
 	$params .= '&' . 'ensGene=full';
+	$params .= '&' . 'knownGene=full';
+	$params .= '&' . 'knownGene.label.gencodeId=1';
+	$params .= '&' . 'knownGene.label.gene=0';
+	$params .= '&' . 'knownGene.label.kgId=0';
+	$params .= '&' . 'knownGene.label.prot=0';
+	$params .= '&' . 'knownGene.label.omimhg38=0';
+	$params .= '&' . 'knownGene.show.comprehensive=1';
+	$params .= '&' . 'knownGene.show.spliceVariants=1';
+	$params .= '&' . 'knownGene.show.noncoding=0';
 	my ($ucsc_render_track_url) = $ENV{APPRIS_WSERVER_UCSC_RENDER_URL} . '?' . $params;
 	my ($ucsc_query_link) = $ENV{APPRIS_WSERVER_UCSC_URL} . '?' . $params;
 	$result = "<a class='imgUCSC' target='_blank' title='Click to alter the display of original UCSC Genome Browser' href='".$ucsc_query_link."'>".
