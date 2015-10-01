9e70f3a74353d34d485e3ed8954241e8471aa5ba
josemrc <josemrc@gmail.com>
2015-10-01 15:28:28 +0200

    Changes in 'downloads'

--- server/app/js/controllers.js
+++ server/app/js/controllers.js
@@ -223,8 +223,9 @@ apprisControllers.controller('DownloadsController', ['$rootScope', '$scope', 'Do
         $scope.isArray = angular.isArray;
 
         // load all data files
+        $scope.urlDOWNLOAD = $rootScope.serverConf.hostWS + '/download/';
         $scope.urlREADME = $rootScope.serverConf.hostWS + '/download/README.txt';
-        $scope.baseUrlDownload = $rootScope.serverConf.hostWS + '/download/releases';
+        $scope.baseUrlDownload = $rootScope.serverConf.hostWS + '/download/current_release';
         $scope.headers = [];
         $scope.datafiles = [];
         $scope.species = [];
