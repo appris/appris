aeac53fe63961dfbcc22eabf199d19129375b9bc
josemrc <josemrc@gmail.com>
2015-10-06 16:33:59 +0200

    The filter of 'codon not found' has been deleted. For cases like GTF3A, or CG43172 gene for drosophila.

--- code/src/appris/appris.pl
+++ code/src/appris/appris.pl
@@ -169,7 +169,6 @@ sub get_scores($$);
 sub get_final_scores($$$);
 sub filter_by_class_codons($$\$\$);
 sub get_annotations($$$);
-#sub is_discarted_by_nmd_codons($);
 sub is_unique($$);
 sub step_appris($$);
 sub step_ccds($$);
@@ -733,17 +732,6 @@ sub filter_by_class_codons($$\$\$)
 			if ( $transcript->biotype and ($transcript->biotype eq 'nonsense_mediated_decay') ) {
 				($frozen_score, $frozen_nscore) = (-1,-1);
 			}
-			if ( $transcript->translate->codons ) {
-				my ($aux_codons) = '';
-				foreach my $codon (@{$transcript->translate->codons}) {
-					if ( ($codon->type eq 'start') or ($codon->type eq 'stop') ) {
-						$aux_codons .= $codon->type.',';							
-					}
-				}
-				unless ( ($aux_codons =~ /start/) and ($aux_codons =~ /stop/) ) {
-					($frozen_score, $frozen_nscore) = (-1,-1);
-				}
-			}
 			push(@{$s_scores->{$frozen_score}}, $transcript_id);
 			$$ref_scores->{$transcript_id}->{$METHOD_LABELS->{$method}->[1]} = $frozen_score;
 			$$ref_nscores->{$transcript_id}->{$method} = $frozen_nscore;
@@ -815,30 +803,6 @@ sub get_annotations($$$)
 	return $annotations;
 	
 } # End get_annotations
-
-#sub is_discarted_by_nmd_codons($)
-#{
-#	my ($transcript) = @_;
-#	my ($discarded) = 0;
-#	
-#	if ( $transcript->biotype and ($transcript->biotype eq 'nonsense_mediated_decay') ) {
-#		$discarded = 1;
-#	}
-#	if ( $transcript->translate->codons ) {
-#		my ($aux_codons) = '';
-#		foreach my $codon (@{$transcript->translate->codons}) {
-#			if ( ($codon->type eq 'start') or ($codon->type eq 'stop') ) {
-#				$aux_codons .= $codon->type.',';							
-#			}
-#		}
-#		unless ( ($aux_codons =~ /start/) and ($aux_codons =~ /stop/) ) {
-#			$discarded = 1;
-#		}
-#	}
-#
-#	return $discarded;
-#	
-#} # end is_discarted_by_nmd_codons
 
 sub is_unique($$)
 {
@@ -932,10 +896,9 @@ sub step_appris($$)
 				$transc_rep->{'tsl'} = 1;
 			}
 			# save the APPRIS decision
-			# discarding the transcripts are not protein cofing (NMD) and condon does not found: app_score is -1
+			# discarding the transcripts are not protein cofing (NMD): app_score is -1
 			# But we accept the last terms when the transcript is unique 
 			my ($unique_transc) = ( scalar(keys(%{$appris_scores})) == 1 and scalar(@{$appris_scores->{$sorted_ap_scores[0]}}) >= 1 ) ? 1 : 0;
-			#if ( ( ($highest_score - $ap_score) <=  $APPRIS_CUTOFF) and ( is_discarted_by_nmd_codons($transcript) == 0 or $unique_transc == 1 ) ) {
 			if ( ( ($highest_score - $ap_score) <=  $APPRIS_CUTOFF) and ( ($ap_score >= 0) or ($unique_transc == 1) ) ) {
 				$transc_rep->{'principal'} = 1;
 				$princ_list->{$transc_id} = 1;
