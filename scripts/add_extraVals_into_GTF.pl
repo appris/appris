0edf9c815a65a79382a227d438f85ecaab1d5df9
josemrc <josemrc@gmail.com>
2016-04-06 10:40:06 +0200

    Add into RefSeq

--- scripts/add_extraVals_into_GTF.pl
+++ scripts/add_extraVals_into_GTF.pl
@@ -21,18 +21,20 @@ use vars qw(
 $LOCAL_PWD					= $FindBin::Bin; $LOCAL_PWD =~ s/bin//;
 
 # Input parameters
+my ($is_refseq) = undef;
+my ($xref_file) = undef;
 my ($data_file) = undef;
 my ($old_data_file) = undef;
-my ($tsl_data_file) = undef;
 my ($out_file) = undef;
 my ($logfile) = undef;
 my ($logpath) = undef;
 my ($logappend) = undef;
 my ($loglevel) = undef;
 &GetOptions(
+	'ref|r'				=> \$is_refseq,
+	'xref=s'			=> \$xref_file,
 	'data=s'			=> \$data_file,
 	'old-data=s'		=> \$old_data_file,
-	'tsl=s'				=> \$tsl_data_file,
 	'outfile=s'			=> \$out_file,
 	'loglevel=s'		=> \$loglevel,
 	'logfile=s'			=> \$logfile,
@@ -75,9 +77,22 @@ sub main()
 	my ($old_genedata) = APPRIS::Parser::_parse_indata($old_data_file);
 	$logger->debug(Dumper($old_genedata)."\n");
 	
-	$logger->info("-- extract TSL list -------\n");
-	my ($tsl_list) = get_tsl_annots($tsl_data_file);
-	$logger->debug(Dumper($tsl_list)."\n");
+	my ($xref_data);
+	my $_extract_dbXref;
+	if ( defined $is_refseq ) {
+		$logger->debug("-- create xref report between RefSeq and Ensembl -------\n");
+		$xref_data = create_xref_ensembl($xref_file);
+		$logger->debug(Dumper($xref_data)."\n");
+		
+		$_extract_dbXref = sub {
+			my ($data,$patt) = @_;
+			my ($match);
+			if ( $data =~ /$patt\:([^\,]*)/ ) {
+				$match = $1;
+			}
+			return $match;
+		};
+	}	
 
 	$logger->info("-- add extra values into GTF -------\n");	
 	open (IN_FILE, $data_file) or throw('Can not open file');
@@ -87,40 +102,69 @@ sub main()
 		unless ( $line =~ /^#/ ) {
 			my ($fields) = APPRIS::Parser::_parse_dataline($line);
 			if ( defined $fields ) {
-				if ( ($fields->{'type'} eq 'transcript') ) {
-					my ($gene_id) = $fields->{'attrs'}->{'gene_id'};
-					my ($transc_id) = $fields->{'attrs'}->{'transcript_id'};
-					$gene_id =~ s/\.[0-9]*$//g;
-					$transc_id =~ s/\.[0-9]*$//g;
-					
-					# add CCDS
-					unless ( $line =~ /ccds_id/ or $line =~ /ccdsid/ ) {
-						if ( exists $old_genedata->{$gene_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'ccdsid'} ) {
-							my ($ccds_id) = $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'ccdsid'};
-							$new_values .= "; ccds_id \"$ccds_id\"";
-						}
-					}					
-					# add RT
-					unless ( $line =~ /"readthrough_transcript"/ ) {
-						if ( exists $old_genedata->{$gene_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'tag'} ) {
-							my ($old_tags) = $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'tag'};
-							foreach my $old_tag ( @{$old_tags} ) {
-								if ( $old_tag eq 'readthrough_transcript' ) {
-									$new_values .= '; tag "readthrough_transcript"';									
+				
+				# RefSeq
+				if ( defined $is_refseq ) {
+					my ($source) = $fields->{'source'};
+					my ($type) = $fields->{'type'};
+					my ($attribs) = $fields->{'attrs'};
+					next unless ( ($source =~ /BestRefSeq/) or ($source =~ /Curated Genomic/) or ($source =~ /Gnomon/) );			
+					if (exists $attribs->{'ID'} ) {
+						my ($gene_id) = $_extract_dbXref->($attribs->{'Dbxref'}, 'GeneID');
+						my ($accesion_id) = $_extract_dbXref->($attribs->{'Dbxref'}, 'Genbank');			
+						# Any type of molecule: mRNA, transcript, ncRNA, etc.
+						if (defined $gene_id and defined $accesion_id and ( $type eq 'transcript' or $type eq 'mRNA' or $type eq 'ncRNA' or $type eq 'primary_transcript' or $type eq 'rRNA' or $type eq 'tRNA') ) {
+							$accesion_id =~ s/\.[0-9]*$//g;
+							# exits cross-refenence with Ensembl
+							if ( exists $xref_data->{$accesion_id} ) {
+								my ($ens_gene_id) = $xref_data->{$accesion_id}->{'gene_id'};
+								my ($ens_transc_id) = $xref_data->{$accesion_id}->{'transc_id'};
+								# add RT
+								if ( exists $old_genedata->{$ens_gene_id} and exists $old_genedata->{$ens_gene_id}->{'transcripts'}->{$ens_transc_id} and exists $old_genedata->{$ens_gene_id}->{'transcripts'}->{$ens_transc_id}->{'tag'} ) {
+									my ($old_tags) = $old_genedata->{$ens_gene_id}->{'transcripts'}->{$ens_transc_id}->{'tag'};
+									foreach my $old_tag ( @{$old_tags} ) {
+										if ( $old_tag eq 'readthrough_transcript' ) {
+											$new_values .= ";tag=readthrough_transcript";									
+										}							
+									}							
 								}
-								
+								# add TSL
+								if ( exists $old_genedata->{$ens_gene_id} and exists $old_genedata->{$ens_gene_id}->{'transcripts'}->{$ens_transc_id} and exists $old_genedata->{$ens_gene_id}->{'transcripts'}->{$ens_transc_id}->{'transcript_support_level'} ) {
+									my ($tsl) = $old_genedata->{$ens_gene_id}->{'transcripts'}->{$ens_transc_id}->{'transcript_support_level'};
+									$new_values .= ";tsl=$tsl";
+								}
 							}
-						}						
-					}
-					# add TSL
-					unless ( $line =~ /"tsl[0-9]"/ ) {
-						if ( exists $tsl_list->{$transc_id} ) {
-							my ($tsl) = $tsl_list->{$transc_id};
-							$new_values .= "; tag \"$tsl\"";
 						}
 					}
-					
-				}				
+				}
+				# Ensembl/GENCODE
+				else {
+					if ( ($fields->{'type'} eq 'transcript') ) {
+						my ($gene_id) = $fields->{'attrs'}->{'gene_id'};
+						my ($transc_id) = $fields->{'attrs'}->{'transcript_id'};
+						$gene_id =~ s/\.[0-9]*$//g;
+						$transc_id =~ s/\.[0-9]*$//g;
+						
+						# add CCDS
+						unless ( $line =~ /ccds_id/ or $line =~ /ccdsid/ ) {
+							if ( exists $old_genedata->{$gene_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'ccdsid'} ) {
+								my ($ccds_id) = $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'ccdsid'};
+								$new_values .= "; ccds_id \"$ccds_id\"";
+							}
+						}					
+						# add RT
+						unless ( $line =~ /"readthrough_transcript"/ ) {
+							if ( exists $old_genedata->{$gene_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id} and exists $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'tag'} ) {
+								my ($old_tags) = $old_genedata->{$gene_id}->{'transcripts'}->{$transc_id}->{'tag'};
+								foreach my $old_tag ( @{$old_tags} ) {
+									if ( $old_tag eq 'readthrough_transcript' ) {
+										$new_values .= '; tag "readthrough_transcript"';									
+									}									
+								}
+							}				
+						}					
+					}					
+				}			
 			}
 		}		
 		$line =~ s/\;*\n*$//g;
@@ -139,20 +183,33 @@ sub main()
 	exit 0;	
 }
 
-sub get_tsl_annots($)
+sub create_xref_ensembl($)
 {
 	my ($file) = @_;
 	my ($report);
+	
+	#9606    5893    ENSG00000002016 NM_001297420.1  ENST00000545564 NP_001284349.1  ENSP00000440268
+	#9606    5893    ENSG00000002016 NM_001297422.1  ENST00000536177 NP_001284351.1  ENSP00000440486
+	#9606    5893    ENSG00000002016 NM_134424.3     ENST00000358495 NP_602296.2     ENSP00000351284
 	my ($flines) = getTotalStringFromFile($file);
-	foreach my $line (@{$flines}) {
-			my (@txt_cols) = split('\t', $line);
-			my ($g_id) = $txt_cols[0];
-			my ($t_id) = $txt_cols[1];
-			my ($tsl_annot) = $txt_cols[2];
-			my ($appris_annot) = $txt_cols[3];
-			if ( $tsl_annot =~ /(tsl[0-9])/ ) {
-				$report->{$t_id} = $1;
-			}			
+	for (my $i=0; $i < scalar(@{$flines}); $i++ ) {
+		my (@cols) = split('\t', $flines->[$i]);
+		my ($tax_id) = $cols[0];
+		my ($entrez_id) = $cols[1];
+		my ($gene_id) = $cols[2];
+		my ($ref_transc_id) = $cols[3];
+		my ($transc_id) = $cols[4];
+		$entrez_id =~ s/\s*//g;
+		$ref_transc_id =~ s/\s*//g;
+		
+		if ( defined $gene_id and defined $transc_id and defined $entrez_id and defined $ref_transc_id and $gene_id =~ /^ENS/ and $transc_id =~ /^ENS/ and $entrez_id ne '' and $ref_transc_id ne '' ) {
+			$ref_transc_id =~ s/\.[0-9]*$//g;
+			$report->{$ref_transc_id} = {
+				'gene_id'	=> $gene_id,
+				'transc_id'	=> $transc_id
+			};
+			#$report->{$ref_transc_id} = $transc_id;
+		}
 	}
 	return $report;
 }
@@ -178,11 +235,13 @@ retrieve_exon_data
 
 =head2 Required arguments:
 
+	--ref  <Flag that indicated RefSeq dataset>
+	
+	--xref=  <Xref file between RefSeq and Ensembl>
+	
 	--data=  <Current Gene annotation file>
 	
 	--old-data=  <Older Gene annotation file with CCDS, RT values>
-	
-	--tsl=  <file with TSL annotations>
 	
 	--outfile <Output Gene annotation file>	
 	
@@ -198,17 +257,31 @@ retrieve_exon_data
 
 =head1 EXAMPLE
 
+=head2 GENCODE/Ensembl example
+
 perl add_extraVals_into_GTF.pl
 	
-	--data={ABSOLUTE_PATH}/Homo_sapiens.GRCh38.84.gtf
+	--data=e84_g24/Homo_sapiens.GRCh38.84.gtf
 	
-	--old-data={ABSOLUTE_PATH}/gencode.v24.annotation.gtf
+	--old-data=e83_g24/gencode.v23.annotation.gtf
 	
-	--tsl=appris/db/TSL.annots.e80_g22_gM5.txt
+	--outfile=e84_g24/Homo_sapiens.GRCh38.84.extra.gtf
 
-	--outfile={ABSOLUTE_PATH}/Homo_sapiens.GRCh38.83.TSL-CCDS-RT.gtf
+=head2 RefSeq example
 
+perl add_extraVals_into_GTF.pl
+	
+	-r
+	
+	--xref=rs107/xref.rs107_ensembl.txt
+	
+	--data=rs107/ref_GRCh38.p2_top_level.gff3
 
+	--old-data=e84_g24/gencode.v24.annotation.gtf
+	
+	--outfile=rs107/ref_GRCh38.p2_top_level.extra.gff3
+	
+	
 =head1 AUTHOR
 
 Jose Manuel Rodriguez Carrasco -jmrodriguez@cnio.es- (INB-GN2,CNIO)
