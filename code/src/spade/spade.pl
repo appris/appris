22e72d14f23e6d61ee481d011da2da6bd5ac651e
appris <appris.cnio@gmail.com>
2016-10-08 12:02:33 +0200

    Cached MD5 - spade

--- code/src/spade/spade.pl
+++ code/src/spade/spade.pl
@@ -107,14 +107,17 @@ sub main()
 			# Run blast
 			$logger->info("\n##Running Pfamscan ---------------\n");
 			my ($pfamscan_sequence_file) = _run_pfamscan($sequence_id, $sequence);
-								                    			                
+								  
+print STDERR "TRANS:\n".Dumper($pfamscan_sequence_file)."\n";
+
 			# Parse blast
 			$logger->info("\n##Parsing Pfamscan ---------------\n");                
 			my ($pfam_report) = _parse_pfamscan($pfamscan_sequence_file, $sequence_id);
 			$transcript_report->{$sequence_id} = $pfam_report if (defined $pfam_report);
 		}
     }
-#	$logger->debug("##CDS coordenates ---------------\n".Dumper($transcript_report));
+    print STDERR "TRANS:\n".Dumper($transcript_report)."\n";
+	$logger->debug("##CDS coordenates ---------------\n".Dumper($transcript_report));
 	
 	# If a transcript has not domains, we check if other transcript has the same sequence with domain (external domains)
 	# But rememeber, we only accept the external domains that do not align with any region domain from the current transcript.
@@ -450,13 +453,13 @@ sub _parse_pfamscan($$)
 	unless( defined $result ) {
 		$logger->error("Can not open pfamscan result: $!\n");
 	}
-	my (@results) = split(/($seq_id[^\n]*\n#HMM[^\n]*\n#MATCH[^\n]*\n#PP[^\n]*\n#SEQ[^\n]*\n+)/,$result);
+	my (@results) = split(/(Query[^\n]*\n#HMM[^\n]*\n#MATCH[^\n]*\n#PP[^\n]*\n#SEQ[^\n]*\n+)/,$result);
 	
 	foreach my $line (@results)
 	{
 		my ($alignment_report);
 
-		if ( $line =~ /^($seq_id[^\n]*)/ )
+		if ( $line =~ /^(Query[^\n]*)/ )
 		{
 			$transcript_result .= '>'.$line;
 				
