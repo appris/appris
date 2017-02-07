c0e44e6deb0da0f25ab8e29b364bfdeb68246038
josemrc <josemrc@gmail.com>
2017-02-07 10:33:22 +0100

    Ensembl script that makes a query to BioMart

--- scripts/create_xref_biomart.pl
+++ scripts/create_xref_biomart.pl
@@ -5,14 +5,51 @@ use strict;
 use LWP::UserAgent;
 
 
-open (FH,$ARGV[0]) || die ("\nUsage: perl webExample.pl Query.xml\n\n");
+#ÊGive the query by input
+#open (FH,$ARGV[0]) || die ("\nUsage: perl webExample.pl Query.xml\n\n");
+#
+#my $xml;
+#while (<FH>){
+#    $xml .= $_;
+#}
+#close(FH);
 
-my $xml;
-while (<FH>){
-    $xml .= $_;
+########################################################
+# USAGE
+#
+my $USAGE =<<USAGE;
+
+Usage:
+
+  perl create_xref_biomart.pl mmusculus [-help]
+    or
+  perl create_xref_biomart.pl drerio [-help]
+         
+Help: Prints out this helpful message
+
+USAGE
+#
+######################################################
+
+if ( !defined $ARGV[0] || $ARGV[0] eq '-h' || $ARGV[0] eq '-help' ) {
+    print "$USAGE\n";
+	exit 0;
 }
-close(FH);
 
+my $species = $ARGV[0];
+
+my $xml = '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" completionStamp = "1">
+
+        <Dataset name = "'.$species.'_gene_ensembl" interface = "default" >
+                <Attribute name = "ensembl_gene_id" />
+                <Attribute name = "external_gene_name" />
+                <Attribute name = "entrezgene" />
+                <Attribute name = "uniprot_swissprot" />
+                <Attribute name = "uniprot_sptrembl" />
+        </Dataset>
+</Query>';
 
 my $path="http://www.ensembl.org/biomart/martservice?";
 my $request = HTTP::Request->new("POST",$path,HTTP::Headers->new(),'query='.$xml."\n");
@@ -30,5 +67,6 @@ $ua->request($request,
 		     warn ("Problems with the web server: ".$response->status_line);
 		 }
 	     },1000);
+
 
 
