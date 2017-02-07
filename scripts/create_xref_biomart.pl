e91724f8934181547da7e77451e1f8254dc8a8c5
josemrc <josemrc@gmail.com>
2017-02-07 10:17:39 +0100

    Ensembl script that makes a query to BioMart

---
+++ scripts/create_xref_biomart.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+# an example script demonstrating the use of BioMart webservice
+use strict;
+use LWP::UserAgent;
+
+
+open (FH,$ARGV[0]) || die ("\nUsage: perl webExample.pl Query.xml\n\n");
+
+my $xml;
+while (<FH>){
+    $xml .= $_;
+}
+close(FH);
+
+
+my $path="http://www.ensembl.org/biomart/martservice?";
+my $request = HTTP::Request->new("POST",$path,HTTP::Headers->new(),'query='.$xml."\n");
+my $ua = LWP::UserAgent->new;
+
+my $response;
+
+$ua->request($request, 
+	     sub{   
+		 my($data, $response) = @_;
+		 if ($response->is_success) {
+		     print "$data";
+		 }
+		 else {
+		     warn ("Problems with the web server: ".$response->status_line);
+		 }
+	     },1000);
+
+
