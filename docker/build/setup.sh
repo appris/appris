a892ec5576183bddf625a07a0c8d749b52b5c1e0
appris <josemrc@gmail.com>
2017-08-28 10:51:19 +0200

    Docker setup

--- docker/build/setup.sh
+++ docker/build/setup.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+# Init MySQL
+/etc/init.d/mysql start
+
 # APPRIS env
 export APPRIS_HOME="/opt/appris"
 source ${APPRIS_HOME}/conf/apprisrc
 source ${APPRIS_HOME}/conf/apprisrc.WS
+
