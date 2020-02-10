e05fbb8bf32478ec6d3dc5a04fdb86ebfc099c8e
Thomas Walsh <twalsh@cnio.es>
2020-02-10 14:58:49 +0100

    Accept optional MANE column in scoring comparison

--- scripts/cmp_scoring_methods.pl
+++ scripts/cmp_scoring_methods.pl
@@ -7,7 +7,6 @@ use File::Find;
 use Getopt::Long;
 use Set::Scalar;
 
-
 my ($a_file) = undef;
 my ($b_file) = undef;
 my ($out_file) = undef;
@@ -24,7 +23,8 @@ my %file_map = (
 );
 
 my %cmp_info;
-my $num_ccds_ids = 0;
+my @known_opt_col_names = ('ccds_id', 'mane_select');
+my %opt_col_map = map { $_ => Set::Scalar->new() } @known_opt_col_names;
 while (my ($trial_key, $trial_file) = each(%file_map) ) {
 
 	open my $fh, $trial_file
@@ -35,6 +35,11 @@ while (my ($trial_key, $trial_file) = each(%file_map) ) {
 	my @field_names = split("\t", $first_line);
 	my %name_to_idx = map { $field_names[$_],  $_ } 0 .. $#field_names;
 
+	my @file_opt_col_names = grep { exists($name_to_idx{$_}) } @known_opt_col_names;
+	foreach my $col_name (@file_opt_col_names) {
+		$opt_col_map{$col_name}->insert($trial_key);
+	}
+
 	while (<$fh>) {
 		my $line = $_;
 		chomp $line;
@@ -43,14 +48,18 @@ while (my ($trial_key, $trial_file) = each(%file_map) ) {
 		my $gene_id = $fields[ $name_to_idx{'gene_id'} ];
 		my $transc_id = $fields[ $name_to_idx{'transc_id'} ];
 		$cmp_info{$gene_id}{$trial_key}{$transc_id} = {
-			'pi_label' => $fields[ $name_to_idx{'pi_label'} ],
-			'ccds_id' => $fields[ $name_to_idx{'ccds_id'} ]
+			'pi_label' => $fields[ $name_to_idx{'pi_label'} ]
 		};
+
+		foreach my $col_name (@file_opt_col_names) {
+			$cmp_info{$gene_id}{$trial_key}{$transc_id}{$col_name} = $fields[ $name_to_idx{$col_name} ];
+		}
 	}
 	close $fh;
 }
 
 my %diff_info;
+my @obs_opt_col_names = grep { $opt_col_map{$_}->size > 0 } @known_opt_col_names;
 while (my($gene_id, $gene_info) = each %cmp_info) {
 
 	next unless ( exists $cmp_info{$gene_id}{'a'} && exists $cmp_info{$gene_id}{'b'} );
@@ -82,9 +91,15 @@ while (my($gene_id, $gene_info) = each %cmp_info) {
 					'gene_id' => $gene_id,
 					'transc_id' => $transc_id,
 					'pi_label_a' => $pi_label_a,
-					'pi_label_b' => $pi_label_b,
-					'ccds_id' => $cmp_info{$gene_id}{'a'}{$transc_id}{'ccds_id'}  # CCDS ID assumed identical for 'a' and 'b'
+					'pi_label_b' => $pi_label_b
 				);
+
+				# TODO: check if optional column values are identical for 'a' and 'b'
+				foreach my $col_name (@obs_opt_col_names) {
+				  my $trial_key = $opt_col_map{$col_name}->[0];
+					$transc_info{$col_name} = $cmp_info{$gene_id}{$trial_key}{$transc_id}{$col_name};
+				}
+
 				$diff_info{$gene_id}{$transc_id} = \%transc_info;
 			}
 		}
@@ -95,17 +110,22 @@ if ( scalar(keys %diff_info) > 0 ) {
 
 	open(my $fh, '>', $out_file)
 		or die("failed to open file '${out_file}'");
-	my @field_names = ('gene_id', 'transc_id', 'pi_label_a', 'pi_label_b', 'ccds_id');
+	my @field_names = ('gene_id', 'transc_id', 'pi_label_a', 'pi_label_b');
+	push(@field_names, @obs_opt_col_names);
 	print $fh join("\t", @field_names)."\n";
 	foreach my $gene_info (values %diff_info) {
 		foreach my $transc_info (values %{$gene_info}) {
+
 			my @row = (
 				$transc_info->{'gene_id'},
 				$transc_info->{'transc_id'},
 				$transc_info->{'pi_label_a'},
-				$transc_info->{'pi_label_b'},
-				$transc_info->{'ccds_id'}
+				$transc_info->{'pi_label_b'}
 			);
+
+			my @opt_col_values = map { $transc_info->{$_} } @obs_opt_col_names;
+			push(@row, @opt_col_values);
+
 			print $fh join("\t", @row)."\n";
 		}
 	}
