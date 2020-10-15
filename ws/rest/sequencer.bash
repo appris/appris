505d624998a6fe665ac50415b9303a8fac3d622b
Thomas Walsh <twalsh@cnio.es>
2020-10-15 17:34:33 +0200

    Execute REST scripts with perlbrew

---
+++ ws/rest/sequencer.bash
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+
+# Env vars of APPRIS web server
+source ${PWD}/../../.bashrc
+source ${PWD}/../../conf/apprisrc
+source ${PWD}/../../conf/apprisrc.WS
+
+# Then, go!
+/opt/perlbrew/bin/perlbrew exec -q --with perl-5.18.2 \
+     perl sequencer.pl "$@"
