e63450937302e9aea4fd8ef6216032615cd80650
Thomas Walsh <twalsh@cnio.es>
2020-10-31 10:47:41 +0100

    Handle PDB-sequence profile suffix in PDB regex

--- server/app/js/browser-seq.js
+++ server/app/js/browser-seq.js
@@ -523,7 +523,7 @@ function createAnnotReferences($filter, query, residues, urlExporter, urlRunnerR
                         if ( angular.isArray(match) && angular.isDefined(match[1]) && angular.isDefined(match[2]) ) {
                             var acc = match[1];
                             var eval = match[2];
-                            var id = acc.replace(/_[A-Z0-9]{1,4}$/i,'');
+                            var id = acc.replace(/_[A-Z0-9]{1,4}(?:_pdb_seq)?$/i,'');
                             annot = "<a href='"+urlPDB+id+"' target='_blank'>"+acc+"</a>" + " (" + eval + ")";
                         }
                     }
